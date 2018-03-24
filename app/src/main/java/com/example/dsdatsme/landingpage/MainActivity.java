459f7c7712c0369df9c7401d2c7bc60b603fe565
DSdatsme <darsuratwala@gmail.com>
2018-03-24 17:53:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/dsdatsme/landingpage/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.dsdatsme.landingpage;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+static final String URL = "https://play.google.com/store/apps/details?id=com.geekgamerz.cardcalculator&hl=en";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        Animation animation;
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.bounce);
+        textView.startAnimation(animation);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+
+            }
+        });
+    }
+
+}
